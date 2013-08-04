74bdf612f4f848ac621173d46f6ee5d7d34b4757
Eric Wright <eric@discoposse.com>
2013-08-04 15:28:48 -0400

    removed swift install script temporarily

--- swift.sh
+++ swift.sh
@@ -3,4 +3,4 @@ sudo apt-get install -y vim git
 git clone https://github.com/discoposse/MasteringOpenStack.git /home/vagrant/MasteringOpenStack
 source /home/vagrant/MasteringOpenStack/openrc
 echo "source /home/vagrant/MasteringOpenStack/openrc" >> ~/.bashrc
-sudo bash /home/vagrant/MasteringOpenStack/swift-install.sh
+# sudo bash /home/vagrant/MasteringOpenStack/swift-install.sh
